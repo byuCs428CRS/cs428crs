b9b0c7f13c73b3b1bcc933f33c49e8190b5b85ac
Leah Loughran <me@leleah.com>
2014-03-06 15:43:14 -0700

    Update SimpleCORSFilter.java

--- src/main/java/controllers/SimpleCORSFilter.java
+++ src/main/java/controllers/SimpleCORSFilter.java
@@ -16,7 +16,7 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Accepts");
 		chain.doFilter(req, res);
 	}
 
