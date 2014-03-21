a47ce8de86bcefd6c7980c42de4a5423300d920b
nhumrich <nhumrich@familysearch.org>
2014-03-21 10:02:43 -0600

    Test new auth fixes

--- src/main/java/controllers/SimpleCORSFilter.java
+++ src/main/java/controllers/SimpleCORSFilter.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import org.apache.catalina.connector.RequestFacade;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,11 @@ public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "http://andyetitcompiles.com");
+    String origin = ((RequestFacade) req).getHeader("Origin");
+    if (origin == null || origin.isEmpty()) {
+      origin = "*";
+    }
+		response.setHeader("Access-Control-Allow-Origin", origin);
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Accepts");
